620245b8a7db4f3998adba519674c9842d2e5d82
remon <remosamir443@gmail.com>
2025-07-21 02:38:10 -0700

    first update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -189,7 +189,7 @@ export function Header() {
                 Contact us
               </Link>
               <Link
-                to="/dashboard"
+                to="/admin"
                 className={`px-3 xl:px-4 py-2 rounded font-semibold transition-colors text-sm xl:text-base ${
                   location.pathname.startsWith("/dashboard")
                     ? "text-gray-900 bg-white"
