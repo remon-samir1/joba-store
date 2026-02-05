fc5ac544cee20e9921a2d041507d3c8e5a039926
remon-samir1 <remosamir443@gmail.com>
2026-02-05 18:07:32 +0200

    update

--- src/pages/dashboard/UpdateProduct.jsx
+++ src/pages/dashboard/UpdateProduct.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DashboardHeader } from "../../components/dashboard/DashboardHeader";
 import {
   Card,
